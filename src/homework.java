785c9bc8ec96523779f3f97cb86217965c968731
stt9 <stt9@njit.edu>
2020-09-16 23:39:33 -0400

    homework1

---
+++ src/homework.java
@@ -0,0 +1,23 @@
+public class homework {
+	public static void main(String[] args) {
+		// If it matches my sample exactly it gets a 0
+		// 1. Create a variable that holds a name
+		String name = "Archer";
+
+		// 2. Output the name to the console
+		System.out.println(name);
+
+		// 3. Create a variable that holds an age (as a whole number)
+		int age = 20;
+
+		// 4. Output the age to the console
+		System.out.println(age);
+
+		// 5. Create a variable that holds a decimal
+		double dec = 0.15;
+
+		// 6. Output the variable
+		System.out.println(dec);
+
+	}
+}
---
+++ src/test.java
@@ -0,0 +1,14 @@
+public class test {
+	public static void main(String[] args) {
+		System.out.println("test");
+	}
+
+	void test() {
+		if (true) {
+			if (false) {
+				System.out.println("no");
+				System.out.println();
+			}
+		}
+	}
+}
