13178dfd9a6cd3475d29f376aaef34676e6be58a
Sean Archer Turqueza <50150118+stt9@users.noreply.github.com>
2020-11-05 21:53:02 -0500

    Add files via upload

--- SocketServer.java
+++ SocketServer.java
@@ -1,37 +1,48 @@
+package server;
 
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-import utils.Debug;
-
-//part 4
 public class SocketServer {
-	int port = 3535;
+	int port = 4545;
 	public static boolean isRunning = false;
 	private List<Room> rooms = new ArrayList<Room>();
 	private Room lobby;// here for convenience
+	private List<Room> isolatedPrelobbies = new ArrayList<Room>();
+	private final static String PRELOBBY = "PreLobby";
+	protected final static String LOBBY = "Lobby";
+	private final static Logger log = Logger.getLogger(SocketServer.class.getName());
 
 	private void start(int port) {
 		this.port = port;
-		Debug.log("Waiting for client");
+		log.log(Level.INFO, "Waiting for client");
 		try (ServerSocket serverSocket = new ServerSocket(port);) {
 			isRunning = true;
 			// create a lobby on start
-			lobby = new Room("Lobby", this);
+			Room.setServer(this);
+			lobby = new Room(LOBBY);// , this);
 			rooms.add(lobby);
 			while (SocketServer.isRunning) {
 				try {
 					Socket client = serverSocket.accept();
-					Debug.log("Client connecting...");
+					log.log(Level.INFO, "Client connecting...");
 					// Server thread is the server's representation of the client
 					ServerThread thread = new ServerThread(client, lobby);
 					thread.start();
-					// add client thread to our room's list of clients
-					lobby.addClient(thread);
-					Debug.log("Client added to clients pool");
+					// create a dummy room until we get further client details
+					// technically once a user fully joins this lobby will be destroyed
+					// but we'll track it in an array so we can attempt to clean it up just in case
+					Room prelobby = new Room(PRELOBBY);// , this);
+					prelobby.addClient(thread);
+					isolatedPrelobbies.add(prelobby);
+
+					log.log(Level.INFO, "Client added to clients pool");
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
@@ -42,16 +53,63 @@ public class SocketServer {
 		} finally {
 			try {
 				isRunning = false;
-				// Thread.sleep(50);
-				Debug.log("closing server socket");
+				cleanup();
+				log.log(Level.INFO, "closing server socket");
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 	}
 
+	protected void cleanupRoom(Room r) {
+		isolatedPrelobbies.remove(r);
+	}
+
+	private void cleanup() {
+		Iterator<Room> rooms = this.rooms.iterator();
+		while (rooms.hasNext()) {
+			Room r = rooms.next();
+			try {
+				r.close();
+			} catch (Exception e) {
+				// it's ok to ignore this one
+			}
+		}
+		Iterator<Room> pl = isolatedPrelobbies.iterator();
+		while (pl.hasNext()) {
+			Room r = pl.next();
+			try {
+				r.close();
+			} catch (Exception e) {
+				// it's ok to ignore this one
+			}
+		}
+		try {
+			lobby.close();
+		} catch (Exception e) {
+			// ok to ignore this too
+		}
+	}
+
 	protected Room getLobby() {
 		return lobby;
+	}
+
+	/***
+	 * Special helper to join the lobby and close the previous room client was in if
+	 * it's marked as Prelobby. Mostly used for prelobby once the server receives
+	 * more client details.
+	 * 
+	 * @param client
+	 */
+	protected void joinLobby(ServerThread client) {
+		Room prelobby = client.getCurrentRoom();
+		if (joinRoom(LOBBY, client)) {
+			prelobby.removeClient(client);
+			log.log(Level.INFO, "Added " + client.getClientName() + " to Lobby; Prelobby should self destruct");
+		} else {
+			log.log(Level.INFO, "Problem moving " + client.getClientName() + " to lobby");
+		}
 	}
 
 	/***
@@ -62,8 +120,12 @@ public class SocketServer {
 	 */
 	private Room getRoom(String roomName) {
 		for (int i = 0, l = rooms.size(); i < l; i++) {
-			if (rooms.get(i).getName().equalsIgnoreCase(roomName)) {
-				return rooms.get(i);
+			Room r = rooms.get(i);
+			if (r == null || r.getName() == null) {
+				continue;
+			}
+			if (r.getName().equalsIgnoreCase(roomName)) {
+				return r;
 			}
 		}
 		return null;
@@ -78,14 +140,17 @@ public class SocketServer {
 	 * @return true if reassign worked; false if new room doesn't exist
 	 */
 	protected synchronized boolean joinRoom(String roomName, ServerThread client) {
+		if (roomName == null || roomName.equalsIgnoreCase(PRELOBBY)) {
+			return false;
+		}
 		Room newRoom = getRoom(roomName);
 		Room oldRoom = client.getCurrentRoom();
 		if (newRoom != null) {
 			if (oldRoom != null) {
-				Debug.log(client.getName() + " leaving room " + oldRoom.getName());
+				log.log(Level.INFO, client.getClientName() + " leaving room " + oldRoom.getName());
 				oldRoom.removeClient(client);
 			}
-			Debug.log(client.getName() + " joining room " + newRoom.getName());
+			log.log(Level.INFO, client.getClientName() + " joining room " + newRoom.getName());
 			newRoom.addClient(client);
 			return true;
 		}
@@ -99,14 +164,17 @@ public class SocketServer {
 	 * @return true if it was created and false if it exists
 	 */
 	protected synchronized boolean createNewRoom(String roomName) {
+		if (roomName == null || roomName.equalsIgnoreCase(PRELOBBY)) {
+			return false;
+		}
 		if (getRoom(roomName) != null) {
 			// TODO can't create room
-			Debug.log("Room already exists");
+			log.log(Level.INFO, "Room already exists");
 			return false;
 		} else {
-			Room room = new Room(roomName, this);
+			Room room = new Room(roomName);// , this);
 			rooms.add(room);
-			Debug.log("Created new room: " + roomName);
+			log.log(Level.INFO, "Created new room: " + roomName);
 			return true;
 		}
 	}
@@ -115,22 +183,18 @@ public class SocketServer {
 		// let's allow port to be passed as a command line arg
 		// in eclipse you can set this via "Run Configurations"
 		// -> "Arguments" -> type the port in the text box -> Apply
-		int port = -1;// make some default
-		if (args.length >= 1) {
-			String arg = args[0];
-			try {
-
-				port = Integer.parseInt(arg);
-			} catch (Exception e) {
-				// ignore this, we know it was a parsing issue
-			}
+		int port = -1;
+		try {
+			port = Integer.parseInt(args[0]);
+		} catch (Exception e) {
+			// ignore this, we know it was a parsing issue
 		}
 		if (port > -1) {
-			Debug.log("Starting Server");
+			log.log(Level.INFO, "Starting Server");
 			SocketServer server = new SocketServer();
-			Debug.log("Listening on port " + port);
+			log.log(Level.INFO, "Listening on port " + port);
 			server.start(port);
-			Debug.log("Server Stopped");
+			log.log(Level.INFO, "Server Stopped");
 		}
 	}
 }
